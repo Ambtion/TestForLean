cc7779522b9401c9e345924b6730ae41d0e59091
LinJunHou <kequ1988@gmail.com>
2020-07-15 11:50:19 +0800

    block

--- TestForLearn/NSMYBlock.h
+++ TestForLearn/NSMYBlock.h
@@ -6,13 +6,14 @@
 //  Copyright © 2020 baidu. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSMYBlock : NSObject
 
-+ (void)testBlock;
+- (void)testBlock;
 
 
 @end
--- TestForLearn/NSMYBlock.m
+++ TestForLearn/NSMYBlock.m
@@ -7,76 +7,26 @@
 //
 
 #import "NSMYBlock.h"
+/*
+  * http://clang.llvm.org/docs/Block-ABI-Apple.html
+  * clang -rewrite-objc MyClass.m
+    support weak https://stackoverflow.com/questions/40946716/how-to-use-weak-reference-in-clang
+  * clang -rewrite-objc -fobjc-arc -stdlib=libc++ -mmacosx-version-min=10.7 -fobjc-runtime=macosx-10.7 -Wno-deprecated-declarations keke.m
 
+ */
 @implementation NSMYBlock
 
-+ (void)testBlock { //global
-    
-    void (^uncatch)(void) = ^{ // __NSGlobalBlock__
-        NSLog(@"block");
-    };
-    
-    static int b = 2;
-    void (^catchLocalStaticArg)(void) = ^{ //__NSGlobalBlock__
-           NSLog(@"block1 %d",b);
-    };
-    
-    NSLog(@"catchLocalStaticArg = %@ ",[catchLocalStaticArg class]);
-
-    static NSString * text2  = @"xxxx";
-    void (^catchLocalStaticStr)(void) = ^{ //__NSGlobalBlock__
-        NSLog(@"block1 %@",text2);
-    };
-    
-    
-    int a = 0;
-    void (^catchLocalArg)(void) = ^{ // __NSMallocBlock__
-        // int a = __cself->a; // bound by copy
-        NSLog(@"block1 %d",a);
-    };
-    
-    NSString * text = @"112";
-    void(^catchLocalString)(void) = ^{ // __NSMallocBlock__
-        // NSString *__strong text = __cself->text; // bound by copy
-        
-        NSLog(@"%s == %@",__FUNCTION__,text);
-    };
-    NSLog(@"catchLocalString = %@ ",[catchLocalString class]);
+- (void)testBlock { //global
     
     __weak typeof(self) weakSelf = self;
-    void (^catchWeak)(void) = ^{ // __NSMallocBlock__ copy_6
-        NSLog(@"block1 %@",weakSelf);
-        // __weak typeof (self) weakSelf = __cself->weakSelf; // bound by copy
-    };
-    NSLog(@"catchWeak = %@ ",[catchLocalString class]);
-
-    NSObject * ob = [NSObject new];
-    void (^catchStrong)(void) = ^{ // __NSMallocBlock__ copy
-        // NSObject *__strong ob = __cself->ob; // bound by copy
-        
-        NSLog(@"block1 %@",ob);
+    int qka = 12;
+    void (^myTestBlok)(void) = ^{
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        NSLog(@"%@ hello %d",strongSelf, qka);
     };
     
-    NSLog(@"catchStrong = %@ ",[catchStrong class]);
-
-    __block int m =  0;
-    void (^cathcBlock)(void) = ^{ // __NSMallocBlock__ copy
-        //   __Block_byref_m_0 *m = __cself->m; // bound by ref
-        m = 2;
-    };
-    
-    NSLog(@"+++cathcBlock %@",[cathcBlock class]);
-    
-//    dispatch_semaphore_t
+    myTestBlok();
 }
-/*
- * 静态全局                 global                           无copy
- * 地址在栈，                malloc block (网上说stack_block) 无copy
- * 地址在堆，                malloc block copy  ，
-                _Block_object_assign函数会根据auto变量的修饰符（__strong、__weak、__unsafe_unretained）做出相应的操作，形成强引用（retain）或者弱引用
-                block_dispose函数内部会调用_Block_object_dispose函数
- *
- */
 
 
 @end
